274b91375a4a2a917f0eeb567337342775e082ff
hsskey <slingshot800@gmail.com>
2018-07-17 22:16:25 +0900

    index.ts 블록체인 예제 추가

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class women {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const ria = new women("herim", 30, "female");
-const whoAmI = (ria) => {
-    return `Hello ${ria.name}, you are ${ria.age} old and  you are ${ria.gender}`;
-};
-console.log(whoAmI(ria));
+const genesisBlock = new Block(0, "125415125", "", "Hello", 20180717);
+let blockchaine = [genesisBlock];
+console.log(blockchaine);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-class women  {
-    public name : string;
-    public age  : number;
-    public gender : string;
-
-    constructor(name:string, age:number, gender:string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+      ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+      }
 }
-const ria = new women("herim",30,"female");
 
-const whoAmI = (ria : women) : string => {
-    return `Hello ${ria.name}, you are ${ria.age} old and  you are ${ria.gender} :)`;
-}
+const genesisBlock:Block = new Block(0,"125415125","","Hello",20180717)
 
-console.log(whoAmI(ria));
+let blockchaine : [Block] = [genesisBlock]
 
-export {};
+console.log(blockchaine);
+export { };
